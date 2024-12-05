2a5a66257e8b5ceed1940ebfe58a20ef0b5c8560
Jeromie Rand <jeromie@gmail.com>
2024-12-04 21:47:27 -0700

    updating solution_stub file

--- 2024/solution_stub.py
+++ 2024/solution_stub.py
@@ -1,40 +1,18 @@
-def parse_file(test=False):
-	filename = 'input.txt'
-	if test:
-		filename = ('test_input.txt')
+def problem_1():
+	""" Do something
+	"""
+	total = 0
+	with open('input.txt') as dataset:
+		return total
 
-	with open(filename) as dataset:
-		# do stuff here
-		return
-
-def problem_1(test=False):
-	data = parse_file(test)
-	return 0
-
-def problem_2(test=False):
-	data = parse_file(test)
-	return 0
-
-def run_tests():
-	problem_1_solution = 0
-	problem_2_solution = 0
-
-	if problem_1(test=True) == problem_1_solution:
-		print("Problem 1 passes the test")
-	else:
-		print("Problem 1 fails the test")
-
-	if problem_2(test=True) == problem_2_solution:
-		print("Problem 2 passes the test")
-	else:
-		print("Problem 2 fails the test")
+def problem_2():
+	""" Do something slightly different
+	"""
+	total = 0
+	with open('input.txt') as dataset:
+		return total
 
 if __name__ == '__main__':
-	run_tests()
-
-	print("The answer to problem 1 is," problem_1())
-	print("The answer to problem 2 is," problem_2())
-
-
-
-
+	print(problem_1())
+	print(problem_2())
+	
