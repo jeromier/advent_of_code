1ee61350b55ba2053f3c288f4a97799d4846986f
Jeromie Rand <jeromie@gmail.com>
2024-12-10 16:08:04 -0700

    Adding (unmodified) week 10

---
+++ 2024/10/week_10.py
@@ -0,0 +1,18 @@
+def problem_1():
+	""" Do something
+	"""
+	total = 0
+	with open('input.txt') as dataset:
+		return total
+
+def problem_2():
+	""" Do something slightly different
+	"""
+	total = 0
+	with open('input.txt') as dataset:
+		return total
+
+if __name__ == '__main__':
+	print(problem_1())
+	print(problem_2())
+	
