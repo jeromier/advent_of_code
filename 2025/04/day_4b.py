176439e3f3b70bcfbd2e55e599fbb66dbeded8b4
Jeromie Rand <jeromie@gmail.com>
2025-12-04 11:48:58 -0700

    Adding a solution using numpy convolution

---
+++ 2025/04/day_4b.py
@@ -0,0 +1,39 @@
+import numpy
+import scipy.signal
+
+def convert_to_digits(roll):
+	if roll == '@':
+		return 1
+	else:
+		return 0
+
+def get_floorplan(filename='example.txt'):
+
+	with open(filename) as map_file:
+		floorplan = numpy.array([list(map(convert_to_digits,line.strip())) for line in map_file.readlines()])
+
+	return floorplan
+
+def get_accessible_rolls(floorplan):
+	counts = scipy.signal.convolve2d(floorplan, numpy.ones((3,3)), mode='same')
+	return (counts*floorplan < 5).sum() - (counts*floorplan == 0).sum()
+
+def get_accessible_rolls_p2(floorplan):
+	counts = scipy.signal.convolve2d(floorplan, numpy.ones((3,3)), mode='same')
+	rolls = (counts*floorplan < 5).sum() - (counts*floorplan == 0).sum()
+	total_rolls = rolls
+	while rolls > 0:
+		floorplan[counts < 5] = 0
+		counts = scipy.signal.convolve2d(floorplan, numpy.ones((3,3)), mode='same')
+		rolls = (counts*floorplan < 5).sum() - (counts*floorplan == 0).sum()
+		total_rolls += rolls
+	return total_rolls
+
+def p1(filename='example.txt'):
+	return get_accessible_rolls(get_floorplan(filename))
+
+def p2(filename='example.txt'):
+	return get_accessible_rolls_p2(get_floorplan(filename))
+
+print(p1('input.txt'))
+print(p2('input.txt'))
